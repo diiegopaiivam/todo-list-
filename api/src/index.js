bee593218dffbbc0131895031297991719317acd
Diego Paiva <diego.paiva@imobsoft.com.br>
2020-04-14 20:29:34 -0300

    modificando porta da aplicação

--- api/src/index.js
+++ api/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
+var porta = process.env.PORT || 3333
 
 const app = express();
 mongoose.connect('mongodb+srv://diegopaiva:lucineide42@cluster0-vc3sa.mongodb.net/todo?retryWrites=true&w=majority', {
@@ -13,4 +14,4 @@ mongoose.connect('mongodb+srv://diegopaiva:lucineide42@cluster0-vc3sa.mongodb.ne
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.listen(3333);
+app.listen(porta);
